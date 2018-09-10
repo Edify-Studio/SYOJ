c3c92de85edeec832901e0dfba01ebd1e0727e5b
NishikinoKKi <2822981379@qq.com>
2018-09-10 18:26:33 +0800

    Update contest.py

--- syzoj/views/contest.py
+++ syzoj/views/contest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from urllib import urlencode
+from urllib import parse
 import time
 from flask import render_template, url_for, request, abort, redirect
 from syzoj import oj, db
@@ -14,7 +14,7 @@ def contest_list():
     query = Contest.query.order_by(db.desc(Contest.start_time))
 
     def make_url(page, other):
-        return url_for("contest_list") + "?" + urlencode({"page": page})
+        return url_for("contest_list") + "?" + parse.urlencode({"page": page})
 
     sorter = Paginate(query, make_url=make_url, cur_page=request.args.get("page"), edge_display_num=3, per_page=10)
     return render_template("contest_list.html", tool=Tools, sorter=sorter, now=time.time())
